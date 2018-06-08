f8ff6889f792b9de177dff42864e258a4897449d
Andrew <latin601@gmail.com>
2018-06-08 11:42:53 +0300

    Aded 404 dud page, changed some animations

---
+++ dist/404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.min.css">
+    <title>Architecture</title>
+</head>
+<body>
+    <?php include_once('includes/header.php');?>
+    <!-- architecture -->
+    <section class="not-found">
+        <div class="">
+            <h1 style="margin-top: 500px; font-size: 60px; text-align: center;">
+                I'm a bee, i'm just a little bee!!!!
+            </h1>
+        </div>
+    </section>
+    <?php include_once('includes/footer.php');?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/common.min.js"></script>
+<script src="js/architecture.min.js"></script>
+</body>
+</html>
