4b4b259a56680969ef8df6b0c9a1bec349861ffc
Andrew <latin601@gmail.com>
2018-06-13 15:55:06 +0300

    finished filter functionality

--- dist/filter.php
+++ dist/filter.php
@@ -37,34 +37,100 @@
         <!-- page-top -->
 
         <div class="filter__controls wrapper">
-
-        <!-- Filter range price total - pt -->
-            <div class="filter__slider filter__slider_pt js-filter__slider_pt">
-                <div class="filter__texts">
-                    <input class="filter__text js-filter__text_min" type="text"/>
-                    <input class="filter__text js-filter__text_max" type="text"/>
+            <div>
+                <!-- Filter range price total - pt -->
+                <div class="filter__slider filter__slider_pt js-filter__slider_pt">
+                    <h5 class="filter__range_heading">Вартість грн:</h5>
+                    <div class="filter__texts">
+                        <input class="filter__text js-filter__text_min" type="text"/>
+                        <input class="filter__text js-filter__text_max" type="text"/>
+                    </div>
+                    <div class="filter__ranges filter__ranges_pt">
+                        <?php /*Place PHP values here*/?>
+                            <input type="range" min="500" max="5000000" class="filter__hidden-values filter__hidden-values_pt js-filter__hidden-values">
+                        <?php /*Place PHP values here*/?>
+                        <input class="filter__range filter__range_pt js-filter__range" type="text">
+                    </div>
                 </div>
-                <div class="filter__ranges filter__ranges_pt">
-                    <?php /*Place PHP values here*/?>
-                        <input type="range" min="500" max="5000000" class="filter__hidden-values filter__hidden-values_pt js-filter__hidden-values">
-                    <?php /*Place PHP values here*/?>
-                    <input class="filter__range filter__range_pt js-filter__range" type="text">
+
+                <!-- Filter range total area - ta -->
+                <div class="filter__slider filter__slider_ta js-filter__slider_ta">
+                    <h5 class="filter__range_heading">Загальна площа м<sup>2</sup>:</h5>
+                    <div class="filter__texts">
+                        <input class="filter__text js-filter__text_min" type="text"/>
+                        <input class="filter__text js-filter__text_max" type="text"/>
+                    </div>
+                    <div class="filter__ranges filter__ranges_ta">
+                        <?php /*Place PHP values here*/?>
+                            <input type="range" min="5" max="100" class="filter__hidden-values filter__hidden-values_ta js-filter__hidden-values">
+                        <?php /*Place PHP values here*/?>
+                        <input class="filter__range filter__range_ta js-filter__range" type="text">
+                    </div>
                 </div>
             </div>
 
-        <!-- Filter range total area - ta -->
-            <div class="filter__slider filter__slider_ta js-filter__slider_ta">
-                <div class="filter__texts">
-                    <input class="filter__text js-filter__text_min" type="text"/>
-                    <input class="filter__text js-filter__text_max" type="text"/>
+            <!-- Rooms checkboxes -->
+            <div class="filter__rooms js-filter__rooms">
+                <h5 class="filter__rooms_heading">К-сть Кімнат:</h5>
+                <div class="filter__room-checkbox filter__room-checkbox_1">
+                    <input id="filter-room_1" type="checkbox" value="1">
+                    <label for="filter-room_1">1</label>
                 </div>
-                <div class="filter__ranges filter__ranges_ta">
-                    <?php /*Place PHP values here*/?>
-                        <input type="range" min="5" max="100" class="filter__hidden-values filter__hidden-values_ta js-filter__hidden-values">
-                    <?php /*Place PHP values here*/?>
-                    <input class="filter__range filter__range_ta js-filter__range" type="text">
+                <div class="filter__room-checkbox filter__room-checkbox_2">
+                    <input id="filter-room_2" type="checkbox" value="2">
+                    <label for="filter-room_2">2</label>
+                </div>
+                <div class="filter__room-checkbox filter__room-checkbox_3">
+                    <input id="filter-room_3" type="checkbox" value="3">
+                    <label for="filter-room_3">3</label>
+                </div>
+                <div class="filter__room-checkbox filter__room-checkbox_4">
+                    <input id="filter-room_4" type="checkbox" value="4">
+                    <label for="filter-room_4">4</label>
                 </div>
             </div>
+
+            <div>
+
+                <!-- Filter range price meter - pm -->
+                <div class="filter__slider filter__slider_pm js-filter__slider_pm">
+                    <h5 class="filter__range_heading">Ціна за м<sup>2</sup>:</h5>
+                    <div class="filter__texts">
+                        <input class="filter__text js-filter__text_min" type="text"/>
+                        <input class="filter__text js-filter__text_max" type="text"/>
+                    </div>
+                    <div class="filter__ranges filter__ranges_pt">
+                        <?php /*Place PHP values here*/?>
+                            <input type="range" min="7000" max="50000" class="filter__hidden-values filter__hidden-values_pt js-filter__hidden-values">
+                        <?php /*Place PHP values here*/?>
+                        <input class="filter__range filter__range_pm js-filter__range" type="text">
+                    </div>
+                </div>
+
+                <!-- Filter range living aera - la -->
+                <div class="filter__slider filter__slider_la js-filter__slider_la">
+                    <h5 class="filter__range_heading">Житлова лоща м<sup>2</sup>:</h5>
+                    <div class="filter__texts">
+                        <input class="filter__text js-filter__text_min" type="text"/>
+                        <input class="filter__text js-filter__text_max" type="text"/>
+                    </div>
+                    <div class="filter__ranges filter__ranges_la">
+                        <?php /*Place PHP values here*/?>
+                            <input type="range" min="12" max="154" class="filter__hidden-values filter__hidden-values_la js-filter__hidden-values">
+                        <?php /*Place PHP values here*/?>
+                        <input class="filter__range filter__range_la js-filter__range" type="text">
+                    </div>
+                </div>
+
+            </div>
+
+            <div>
+                За вашими параметрами є <span class="filter__results-found-numb"></span> квартир
+            </div>
+
+            <a href="#" class="filter__btn btn js-filter__btn">Шукати</a>
+
+            <a href="#" class="filter__reset js-filter__reset">Зкинути параметри</a>
 
         </div>
 
@@ -74,13 +140,17 @@
                     data-pricetotal="10000"
                     data-totalarea="40"
                     data-rooms="1"
-                    class="filter__result js-filter__result">
+                    data-pricemeter="11000"
+                    data-livingarea="20"
+                    class="filter__result js-filter__result filter__result_hidden">
                     <a href="#">
                         <div>
                             <div>Result 1</div>
                             <div>Price total 10000</div>
                             <div>Total area 40</div>
                             <div>Rooms 1</div>
+                            <div>Price meter 11000</div>
+                            <div>Living area 20</div>
                         </div>
                     </a>
                 </li>
@@ -88,13 +158,17 @@
                     data-pricetotal="20000"
                     data-totalarea="20"
                     data-rooms="2"
-                    class="filter__result js-filter__result">
+                    data-pricemeter="12000"
+                    data-livingarea="30"
+                    class="filter__result js-filter__result filter__result_hidden">
                     <a href="#">
                         <div >
                             <div>Result 2</div>
                             <div>Price total 20000</div>
                             <div>Total area 20</div>
                             <div>Rooms 2</div>
+                            <div>Price meter 12000</div>
+                            <div>Living area 30</div>
                         </div>
                     </a>
                 </li>
@@ -102,13 +176,17 @@
                     data-pricetotal="60000"
                     data-totalarea="80"
                     data-rooms="3"
-                    class="filter__result js-filter__result">
+                    data-pricemeter="13000"
+                    data-livingarea="40"
+                    class="filter__result js-filter__result filter__result_hidden">
                     <a href="#">
                         <div >
                             <div>Result 3</div>
                             <div>Price total 60000</div>
                             <div>Total area 80</div>
                             <div>Rooms 3</div>
+                            <div>Price meter 13000</div>
+                            <div>Living area 40</div>
                         </div>
                     </a>
                 </li>
@@ -116,13 +194,17 @@
                     data-pricetotal="5000"
                     data-totalarea="10"
                     data-rooms="1"
-                    class="filter__result js-filter__result">
+                    data-pricemeter="14000"
+                    data-livingarea="50"
+                    class="filter__result js-filter__result filter__result_hidden">
                     <a href="#">
                         <div>
                             <div>Result 4</div>
                             <div>Price total 5000</div>
                             <div>Total area 10</div>
                             <div>Rooms 1</div>
+                            <div>Price meter 14000</div>
+                            <div>Living area 50</div>
                         </div>
                     </a>
                 </li>
@@ -130,13 +212,17 @@
                     data-pricetotal="2000"
                     data-totalarea="90"
                     data-rooms="4"
-                    class="filter__result js-filter__result">
+                    data-pricemeter="15000"
+                    data-livingarea="60"
+                    class="filter__result js-filter__result filter__result_hidden">
                     <a href="#">
                         <div>
                             <div>Result 5</div>
                             <div>Price total 20000</div>
                             <div>Total area 90</div>
                             <div>Rooms 4</div>
+                            <div>Price meter 15000</div>
+                            <div>Living area 60</div>
                         </div>
                     </a>
                 </li>
@@ -144,18 +230,26 @@
                     data-pricetotal="300000"
                     data-totalarea="50"
                     data-rooms="2"
-                    class="filter__result js-filter__result">
+                    data-pricemeter="16000"
+                    data-livingarea="70"
+                    class="filter__result js-filter__result filter__result_hidden">
                     <a href="#">
                         <div>
                             <div>Result 6</div>
                             <div>Price total 300000</div>
                             <div>Total area 50</div>
                             <div>Rooms 2</div>
+                            <div>Price meter 16000</div>
+                            <div>Living area 70</div>
                         </div>
                     </a>
                 </li>
             </ul>
+
+                    <a href="#" class="filter__more-results-btn btn js-filter__more-results">Дивитися більше планувань</a>
+
         </div>
+
 
                  
 
--- src/js/filter.js
+++ src/js/filter.js
@@ -1,15 +1,31 @@
 var filterPage = (function() {
 
     // All functionality is built around jQuery.... Wont work without it!
-
     var ranges = [];
     var filter = {
-        rooms: [1, 4]
+        rooms: [1, 4, 3, 2]
     };
+
     var results = document.querySelectorAll('.js-filter__result');
     var hiddenResultsClass = 'filter__result_hidden';
     var visibleResultsClass = 'filter__result_visible';
-    var resultsPerSearch = 2;
+    var filteredResults = [];
+    // Show per new search
+    var defaultSearchNumber = 2;
+    var resultsPerSearch = defaultSearchNumber;
+    var moreResults = 2;
+    // More results button
+    var moreResultsBtn = document.querySelector('.js-filter__more-results');
+    // Container with checkboxes for change event
+    var filterRoomsCheckboxes = document.querySelector('.js-filter__rooms');
+    // Checkboxxes themselfes
+    var checkBoxes = filterRoomsCheckboxes.querySelectorAll('input[type="checkbox"]');
+    // filter btn
+    var filterBtn = document.querySelector('.js-filter__btn');
+    // found
+    var resultsFoundNumber = document.querySelector('.filter__results-found-numb');
+    // filter reset btn
+    var filterResetBtn = document.querySelector('.js-filter__reset');
 
     // range constructor responsible for updating ranges, updating text inputs, updating global filter state 
     function Range(parentSelector, type) {
@@ -27,7 +43,6 @@ var filterPage = (function() {
                 min: slider.old_from !== undefined ? slider.old_from : self.min,
                 max: slider.old_to !== undefined ? slider.old_to : self.max
             }
-            console.log(filter);
         };
 
         // Set values for text inputs on initialisation
@@ -96,28 +111,32 @@ var filterPage = (function() {
 
     var priceTotalRange = new Range('.js-filter__slider_pt', 'pricetotal');
     var areaTotalRange = new Range('.js-filter__slider_ta', 'totalarea');
+    var priceMeterRange = new Range('.js-filter__slider_pm', 'pricemeter');
+    var livingAreaRange = new Range('.js-filter__slider_la', 'livingarea');
+
     ranges.push(priceTotalRange);
     ranges.push(areaTotalRange);
+    ranges.push(priceMeterRange);
+    ranges.push(livingAreaRange);
 
-
-    var i = 0;
     function filterResults() {
+        resultsPerSearch = defaultSearchNumber;
+        filteredResults = [];
+
         results.forEach(function(result) {
-            var passed = null;
+
+            var passed = false;
             var resultObject = $(result).data();
+
             for(var key in filter) {
                 if(!($.isArray(filter[key]))) {
-                    //Value is Object literal
+                    // Value is Object literal
                     var val = resultObject[key];
-                    if(filter[key].min < val && filter[key].max > val) {
-                        // passed min max filters
-                        result.classList.remove(hiddenResultsClass);
-                        result.classList.add(visibleResultsClass);
+                    if(filter[key].min <= val && filter[key].max >= val) {
+                        // Passed min max filters
                         passed = true;
                     } else {
-                        // not passed min max filters
-                        result.classList.remove(visibleResultsClass);
-                        result.classList.add(hiddenResultsClass);
+                        // Not passed min max filters
                         passed = false;
                         break;
                     }
@@ -125,25 +144,91 @@ var filterPage = (function() {
                     // Value is array
                     if(filter[key].indexOf(resultObject[key])!==-1) {
                         // Found number of rooms in array
-                        result.classList.remove(hiddenResultsClass);
-                        result.classList.add(visibleResultsClass);
                         passed = true;
                     } else {
                         // Not found number of rooms in array
-                        result.classList.remove(visibleResultsClass);
-                        result.classList.add(hiddenResultsClass);
                         passed = false;
                         break;
                     }
                 }
             }
 
+            // If passed all filters result goes into filtered array
             if(passed) {
-                i++;
+                filteredResults.push(result);
             }
 
-        });
+        }); // End foreach
+        resultsFoundNumber.innerHTML = filteredResults.length;
+        hideAllResults();
+        showFiltered();
     };
     filterResults();
-    console.log(i);
+
+    // Hide all results
+    function hideAllResults() {
+        results.forEach(function(result){
+            result.classList.remove(visibleResultsClass);
+            result.classList.add(hiddenResultsClass);
+        });
+    };
+
+    // Show more results function creation 
+    function showFiltered() {
+        filteredResults.forEach(function(result, index) {
+            if(index  < resultsPerSearch) {
+                result.classList.remove(hiddenResultsClass);
+                result.classList.add(visibleResultsClass);
+            }
+        });
+    };
+
+    // Checkboxes change event
+    filterRoomsCheckboxes.addEventListener('change', function(){
+        filter.rooms = [];
+        checkBoxes.forEach(function(checkbox) {
+            if(checkbox.checked) {
+                filter.rooms.push(parseInt(checkbox.value));
+            }
+        });
+        // If no checkboxes selected show all rooms
+        if(filter.rooms.length === 0) {
+            resetCheckboxes();
+        }
+    });
+
+    function resetCheckboxes() {
+        checkBoxes.forEach(function(checkbox) {
+            checkbox.checked = false;
+            filter.rooms.push(parseInt(checkbox.value));
+        });
+    };
+
+    moreResultsBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        resultsPerSearch = resultsPerSearch + moreResults;
+        showFiltered();
+    });
+
+    filterBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        filterResults();
+    });
+
+    filterResetBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        ranges.forEach(function(range) {
+            var slider = range.ionRange.data("ionRangeSlider");
+            slider.update({
+                from: range.min,
+                to: range.max
+            });
+            range.updateFiler();
+            $(range.minText).val(range.min);
+            $(range.maxText).val(range.max);
+            resetCheckboxes();
+            filterResults();
+        })
+    })
+
 })();
